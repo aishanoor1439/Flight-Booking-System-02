3c6fd38bbb79db622f1cd77ddd0617dba49e9d46
aishanoor1439 <aisha.noor1439@gmail.com>
2025-05-12 21:49:24 +0500

    First Commit
    added project files

---
+++ src/main/java/com/mycompany/mavenproject41/Mavenproject41.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject41;
+
+/**
+ *
+ * @author ABC
+ */
+public class Mavenproject41 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
